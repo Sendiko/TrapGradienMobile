ec5f8baac9bd1b4be4f94c34706899e4c629dcb4
Sendiko <sarangtawon897@gmail.com>
2025-12-03 15:53:21 +0700

    chore: Remove unused color values for light and dark themes
    - Cleaned up redundant medium and high contrast color definitions.
    - Removed unused `unspecified_scheme` ColorFamily definition.
    This simplifies the theming system by retaining only relevant configurations.

--- composeApp/src/commonMain/kotlin/id/my/gradien/cloud/core/ui/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/id/my/gradien/cloud/core/ui/theme/Theme.kt
@@ -91,10 +91,6 @@ data class ColorFamily(
     val onColorContainer: Color
 )
 
-val unspecified_scheme = ColorFamily(
-    Color.Unspecified, Color.Unspecified, Color.Unspecified, Color.Unspecified
-)
-
 @Composable
 expect fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
